dd1e8bbc9db50c37329f5e273d9a441bdb8c5329
jiroblea <jirooblea@gmail.com>
2021-11-30 16:30:54 +0800

    tried to make the buttons working, it still doesn't

--- HistoryPuzzle/app/src/main/java/com/example/historypuzzle/MainActivity.java
+++ HistoryPuzzle/app/src/main/java/com/example/historypuzzle/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.historypuzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // TODO: make the buttons working
+    Button btn_howToPlay, btn_aboutUs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_howToPlay = findViewById(R.id.button_HowToPlay);
+        btn_aboutUs = findViewById(R.id.button_AboutUs);
+
+        btn_howToPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HowToPlay.class));
+            }
+        });
+
+        btn_aboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AboutUs.class));
+            }
+        });
     }
 }
